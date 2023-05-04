b91aa31a00784e9c04ee4befda09ec941ddd6c1e
Guoming Yang <ming@ming.email>
2023-05-03 21:37:34 -0500

    use chrono::Local

--- rust/perspective-viewer/src/rust/utils/datetime.rs
+++ rust/perspective-viewer/src/rust/utils/datetime.rs
@@ -6,7 +6,7 @@
 // of the Apache License 2.0.  The full license can be found in the LICENSE
 // file.
 
-use chrono::{DateTime, FixedOffset, NaiveDateTime, TimeZone, Utc};
+use chrono::{Local, NaiveDateTime, TimeZone};
 use wasm_bindgen::prelude::*;
 
 use crate::utils::*;
@@ -20,25 +20,17 @@ fn input_value_format(x: &str) -> Result<&str, JsValue> {
     }
 }
 
-fn get_local_tz() -> FixedOffset {
-    FixedOffset::west(js_sys::Date::new(&0.into()).get_timezone_offset() as i32 * 60)
-}
-
 pub fn posix_to_utc_str(x: f64) -> ApiResult<String> {
-    let tz = get_local_tz();
     if x > 0_f64 {
-        Ok(Utc
-            .timestamp(x as i64 / 1000, ((x as i64 % 1000) * 1000000) as u32)
-            .with_timezone(&tz)
-            .format("%Y-%m-%dT%H:%M:%S%.3f")
-            .to_string())
+        let dt = Local.timestamp_millis(x as i64);
+        Ok(dt.to_rfc3339_opts(chrono::SecondsFormat::Millis, true))
     } else {
         Err(format!("Unknown timestamp {}", x).into())
     }
 }
 
 pub fn str_to_utc_posix(val: &str) -> Result<f64, ApiError> {
-    let tz = get_local_tz();
     let posix = NaiveDateTime::parse_from_str(val, input_value_format(val)?)?;
-    Ok(DateTime::<Utc>::from(tz.from_local_datetime(&posix).unwrap()).timestamp_millis() as f64)
+    let dt = Local.from_local_datetime(&posix).unwrap();
+    Ok(dt.timestamp_millis() as f64)
 }
